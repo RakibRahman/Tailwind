7097f5631b2c3d6c4c1e6399c92913c50dd9e106
RakibRahman <md.rr.talukder@gmail.com>
2021-02-03 21:57:55 +0600

    Tailwind Config & Custom Font Setup

---
+++ exercises/misc/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        hand: ["Shadows Into Light", "cursive"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
