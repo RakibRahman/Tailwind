fb760a43458a19ecf6d3fe3ec16028ff6d0702be
RakibRahman <md.rr.talukder@gmail.com>
2021-02-09 17:12:37 +0600

    Hover, Focus, & Other States

--- exercises/animate/tailwind.config.js
+++ exercises/animate/tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        "pulse-fast": "pulse 1s linear infinite",
+        "bounce-fast": "bounce 1s linear infinite",
+        wiggle: "wiggle 2s linear infinite",
+      },
+      keyframes: {
+        wiggle: {
+          "0%": { transform: "rotate(-15deg)" },
+          "50%": { transform: "rotate(15deg)" },
+          "100%": { transform: "rotate(-15deg)" },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
